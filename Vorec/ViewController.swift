73a2166adb0e84b2c848297d19fe361a03c50075
Rohan Roy <rohan@rohanroy.com>
2016-06-19 22:49:24 +0530

    Initial Commit

---
+++ Vorec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vorec
+//
+//  Created by Rohan Roy on 19/06/16.
+//  Copyright © 2016 Rohan Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
